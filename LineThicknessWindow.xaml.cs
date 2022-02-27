7125ddac92a0f0641d6e7fbe7d6d83a2c52dbaab
bjyoung <bjyoung20591@gmail.com>
2022-02-26 21:12:55 -0500

    Shift open curly braces one line above
    For consistency.

--- LineThicknessWindow.xaml.cs
+++ LineThicknessWindow.xaml.cs
@@ -2,43 +2,34 @@
 using System.Windows;
 using System.Windows.Controls;
 
-namespace Horizontal_Guide
-{
+namespace Horizontal_Guide{
     // Sub-window displaying line-thickness
     // TODO change Window1 to LineThicknessWindow
-    public partial class Window1 : CloseableWindow
-    {
+    public partial class Window1 : CloseableWindow{
         // Constructor
-        public Window1()
-        {
+        public Window1(){
             InitializeComponent();
         }
 
         // Close window when 'OK' button is clicked
-        private void OKButton_OnClick(object sender, RoutedEventArgs e)
-        {
+        private void OKButton_OnClick(object sender, RoutedEventArgs e){
             // Close current window when clicked and send value back to original window
             Close();
         }
 
         // Adjust line thickness as dropdown list value is changed
-        private void LineThicknessCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+        private void LineThicknessCombo_SelectionChanged(object sender, SelectionChangedEventArgs e){
             // If user selects a new value from the list, update the line thickness in main
             ComboBoxItem combo_box_item = ((sender as ComboBox).SelectedItem as ComboBoxItem);
             string selected_text = combo_box_item.Content.ToString();
 
-            if (selected_text != "")
-            {
+            if (selected_text != ""){
                 int new_line_thickness = int.Parse(selected_text);
 
-                try
-                {
+                try{
                     ((MainWindow)Application.Current.MainWindow).UpdateLineThickness(new_line_thickness);
 
-                }
-                catch (Exception ex)
-                {
+                }catch (Exception ex){
                     // Do nothing, just to catch exception in case main window closes
                 }
             }
