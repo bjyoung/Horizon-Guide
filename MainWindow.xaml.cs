f004c4c1c1a29e97918de01d061098b44d65a021
bjyoung <bjyoung20591@gmail.com>
2022-03-26 21:23:35 -0400

    Organize using imports based on C# style guide

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Shapes;
-using System.Windows.Controls.Primitives;
-using WpfScreenHelper;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using WpfScreenHelper;
 
-namespace HorizontalGuide{
+namespace HorizontalGuide {
     // Initial window with the adjustable line and UI buttons
     public partial class MainWindow : CloseableWindow{
 
